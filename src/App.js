9bec1e157203fbd549b751269cf1e1b9536109e4
ShroatMehta <shroatm1234@gmail.com>
2020-09-01 01:17:39 +0530

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>TEDxMahe</h1>
+        
+        <p>
+          Work in Progress
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
