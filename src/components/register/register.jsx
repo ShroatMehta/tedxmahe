1fbfae86fea4316b6972fe4a170a094b2867347b
ShroatMehta <shroatm1234@gmail.com>
2020-09-09 11:21:21 +0530

    register

---
+++ src/components/register/register.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './register.styles.css';
+export const Register = ()=>(
+    <div className = "register">
+        <h1 className="heading">#jointhecountdown</h1>
+
+    <a href="#" className="button"> Register as a Team</a>
+    <a href="#" className="button"> Register as an Individual</a>
+        
+    </div>
+)
+export default Register;
---
+++ src/pages/register/registerpage.page.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import './registerpage.styles.scss'
+import Register from '../../components/register/register.jsx'
+export const RegisterPage = () =>(
+    <div className = "Register-page">
+    
+    <Register/>
+        
+    </div>
+)
+
+export default RegisterPage;
